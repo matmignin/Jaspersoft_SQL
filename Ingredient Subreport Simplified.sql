e559d5f4ebb4d0c3408653027a7a4c9eee4aa9e5
matmignin <mat@mignin.com>
2022-07-02 05:07:57 -0400

    updated

--- Ingredient Subreport Simplified.sql
+++ Ingredient Subreport Simplified.sql
@@ -9,16 +9,16 @@ SELECT
     ir.method,
     ir.generic01 AS notebook_ref,
    	ir.status,
-    
-    
+
+
     -- Ingredient name/description
-    -- Handling The Generic Ingredients A-Q + mulitpart list 
+    -- Handling The Generic Ingredients A-Q + mulitpart list
     CASE -- if i.IngredientId contain Generic Ingredient ___ then combine it with the discription from the other i.IngredientIds that also contain Generic ingredient**
         WHEN UPPER(i.ingredientid) LIKE 'MULTI-PART INGREDIENT LIST%'
         THEN -- then combine it with other rows that contain "MULTI...""
             (
              SELECT LISTAGG(i2.description) --aggrigates a multiple rows together into one and orders it
-             WITHIN GROUP (ORDER BY i2.ingredientid) -- then orders 
+             WITHIN GROUP (ORDER BY i2.ingredientid) -- then orders
              FROM ingredient i2 -- referenceing the ingredientid twice
              WHERE UPPER(i2.ingredientid) LIKE 'MULTI%' -- if ingredientid starts with "Multi"
              AND i2.formulationguid = i.formulationguid -- and if the rows have matching formulationguid
@@ -35,11 +35,11 @@ SELECT
              WHERE UPPER(ingredientid) LIKE 'GENERIC INGREDIENT Q%' AND i2.formulationguid = i.formulationguid)
         ELSE i.description --if the ingredient.Description doesn't have Generic ingredient, the just return the discription
     END AS ingredient, --call the output of this "loop" Ingredient
-  
-  
+
+
     -- Result display value
     CASE
-        -- format any results that numeric, relased, not skiploted or blank 
+        -- format any results that numeric, relased, not skiploted or blank
         WHEN ir.resulttype = 'NUMERIC' AND ir.status = 70 AND (ir.resultvaluation <> 'SKIP LOT' OR ir.resultvaluation IS NULL)
         -- format the result to be "<Prefix>comma-separated digetsand add commas
         THEN ir.prefix||TRIM(TO_CHAR(REGEXP_SUBSTR(REPLACE(ir.numericalresulttext,ir.prefix,''),'^\d+'),'999,999,999'))
@@ -49,7 +49,7 @@ SELECT
                  THEN SUBSTR(ir.numericalresulttext,INSTR(ir.numericalresulttext,'.'))
                  END
                 )
-                -- if Unit is Blank, the return blank, 
+                -- if Unit is Blank, the return blank,
              || DECODE(ir.unit,NULL,NULL,
                     ' '||ir.unit) -- else return a space and unit
         WHEN ir.resulttype IN ('LIST','TEXT')
@@ -77,10 +77,10 @@ SELECT
                     i.generic3
                 ELSE 'Not Tested'
             END
-    END AS result
+    END AS result --{RESULT}
 FROM
     -- Ingredients for this product/formulation
-    ingredient i
+    ingredient i --[i] ingredient
     JOIN formulation f ON i.formulationguid = f.formulationguid
             AND f.productid =  $P{PRODUCTID}
             AND f.formulationid  =  $P{FORMULATIONID}
@@ -102,7 +102,7 @@ FROM
                     THEN
                       DECODE(tr.resulttype,NULL,NULL,tr.requirement)
                     ELSE  ''
-                 END AS specification_range,
+                 END AS specification_range, --{SPECIFICATION_RANGE}
                 -- Method
                 CASE
                     WHEN (tr.resultvaluation <> 'SKIP LOT'
@@ -110,8 +110,8 @@ FROM
                     THEN
                         sm.description
                     ELSE  ''
-                END AS method
-        FROM testresult tr
+                END AS method --{METHOD}
+        FROM testresult tr --[tr] test result and [t] test
         JOIN test t ON t.testguid = tr.testguid
             AND t.deletion = 'N'
             AND t.requestguid IN
@@ -124,11 +124,13 @@ FROM
           LEFT JOIN smmethod sm ON sm.methodid = t.methodid
           AND sm.versionno = t.methodversionno
           AND sm.deletion = 'N'
+
           LEFT JOIN testresultrequirement trr ON trr.resultguid = tr.resultguid
           AND trr.valuationcode = 1
           WHERE tr.deletion = 'N'
           AND tr.flagisfinalresult = 'Y'
           AND configurationid <> 'Stability' -- (1.3.3) Fix Stability from showing up on the CoA
+          
         ) ir
     ON UPPER(i.ingredientid) = UPPER(ir.resultid)
 WHERE i.deletion = 'N'
